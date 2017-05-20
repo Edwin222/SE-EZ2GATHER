9d7163c15c503f4ced12b487fee400bdd5b25c69
unknown <comeng@B01.com.knu.ac.kr>
2017-05-20 18:50:14 +0900

    PKDProtocol

--- Scheduler_Client/src/client/NetClient.java
+++ Scheduler_Client/src/client/NetClient.java
@@ -1,5 +1,51 @@
+package client;
 
+import java.net.*;
+import java.io.*;
 
 public class NetClient {
+	private String IP;
+	private int port;
+	private Socket socket;
 	
+	public NetClient(String IP, int port){
+		this.IP = IP;
+		this.port = port;
+	}
+	
+	public void connectToServer(){
+		try {
+			socket = new Socket(IP, port);
+			
+		} catch(Exception e){
+			System.err.println("Connection error");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void sendMessage(String msg){
+		try {
+			
+			DataOutputStream sock_out = new DataOutputStream(socket.getOutputStream());
+			DataInputStream sock_in = new DataInputStream(socket.getInputStream());
+			
+			//in & out execute
+			sock_out.writeUTF(msg);
+			
+			sock_out.close();
+			sock_in.close();
+			
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void endConnection() {
+		try {
+			socket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- Scheduler_Server/src/Server/NetServer.java
+++ Scheduler_Server/src/Server/NetServer.java
@@ -1,4 +1,91 @@
+package Server;
+
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
 
 public class NetServer {
+	
+	//singleton
+	private static class Singleton {
+		private static final NetServer Instance = new NetServer();
+	}
+	
+	public static NetServer getInstance(){
+		return Singleton.Instance;
+	}
+	
+	//constant
+	private final int THREAD_NUM = 8;
+	private final int PORT_NUM = 5000;
+	
+	//implementation
+	private ExecutorService threadPool;
+	private ServerSocket server;
+	
+	private NetServer(){
+		threadPool = Executors.newFixedThreadPool(THREAD_NUM);
+	}
+	
+	public void openServer(){
+		try {
+			server = new ServerSocket(PORT_NUM);
+		} catch(Exception e){
+			System.err.println("Server open error.");
+			e.printStackTrace();
+		}
+		
+		Runnable task = new Runnable(){
+			@Override
+			public void run(){
+				System.out.println("Waiting Client in " + Thread.currentThread().getName() + "...");
+				try {
+					Socket client_socket = server.accept();
+					//after connection
+					System.out.println("Client Connected : " + client_socket.getInetAddress());
+					
+					DataOutputStream sock_out = new DataOutputStream(client_socket.getOutputStream());
+					DataInputStream sock_in = new DataInputStream(client_socket.getInputStream());
+					
+					//in & out execute
+					String msg = sock_in.readUTF();
+					
+					if(msg.equals("LOGIN")){
+						// LOGIN : check date & send table information
+						
+					} else if(msg.equals("REFRESH")){
+						// REFRESH : check date & send table information
+						
+					} else if(msg.equals("SAVE")){
+						// SAVE : check date & modify table information & send table information
+						
+					}
+					
+					sock_out.close();
+					sock_in.close();
+					
+					//end connection
+					client_socket.close();
+					
+				} catch(Exception e){
+					e.printStackTrace();
+					
+				}
+			}
+		};
+		
+		for(int i=0;i<THREAD_NUM;i++){
+			threadPool.submit(task);
+		}
+	}
+	
+	public void closeServer(){
+		try {
+			threadPool.shutdown();
+			server.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
